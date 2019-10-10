d406b114c3a3e0f7162d83cc552d88cc51ec5475
Annusza <polonistyka2@o2.pl>
2019-10-10 16:00:33 +0200

    addBookTest commit

--- src/main/java/com/annusza/tau/lab01/myBookApp/service/BookManagerImpl.java
+++ src/main/java/com/annusza/tau/lab01/myBookApp/service/BookManagerImpl.java
@@ -1,33 +1,41 @@
 package com.annusza.tau.lab01.myBookApp.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.annusza.tau.lab01.myBookApp.domain.Book;
 
 public class BookManagerImpl implements BookManager {
 
+	List<Book> books = new ArrayList<Book>();
+
 	public void addBook(Book book) {
 
-		// TODO Auto-generated method stub
-		
+		books.add(book);
+
 	}
 
 	public Book getBookById(int id) {
 
-		// TODO Auto-generated method stub
+		for (Book book : books) {
+			if (book.getId() == id) {
+
+				return book;
+			}
+		}
 		return null;
 	}
 
 	public void updateBook(Book book) {
 
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void deleteBook(int id) {
 
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public List<Book> getAllGames() {
--- src/test/java/com/annusza/tau/lab01/myBookApp/test/BookManagerTest.java
+++ src/test/java/com/annusza/tau/lab01/myBookApp/test/BookManagerTest.java
@@ -1,20 +1,41 @@
 package com.annusza.tau.lab01.myBookApp.test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import com.annusza.tau.lab01.myBookApp.domain.Book;
+import com.annusza.tau.lab01.myBookApp.service.BookManagerImpl;
+
 @RunWith(JUnit4.class)
 public class BookManagerTest {
-	
+
+	BookManagerImpl bookManagerImpl = new BookManagerImpl();
+
 	@Test
 	public void dummyTest() {
-		
+
 		Assert.assertTrue(true);
-		
+
 	}
-	
-	
+
+	@Test
+	public void addBookShouldAddNewBook() {
+
+		Book maus = new Book();
+		maus.setId(1);
+		maus.setAuthorName("Art");
+		maus.setAuthorSurname("Spiegelman");
+		maus.setTitle("Maus");
+		maus.setYearOfPublication(2009);
+
+		bookManagerImpl.addBook(maus);
+
+		assertEquals(bookManagerImpl.getBookById(1).getTitle(), maus.getTitle());
+
+	}
 
 }
