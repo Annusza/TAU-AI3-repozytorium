ab83c57cfab059554e8a2a290e38dad1654af628
Annusza <polonistyka2@o2.pl>
2019-10-10 15:31:17 +0200

    dummyTest commit

--- src/test/java/com/annusza/tau/lab01/myBookApp/test/BookManagerTest.java
+++ src/test/java/com/annusza/tau/lab01/myBookApp/test/BookManagerTest.java
@@ -1,6 +1,20 @@
 package com.annusza.tau.lab01.myBookApp.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class BookManagerTest {
+	
+	@Test
+	public void dummyTest() {
+		
+		Assert.assertTrue(true);
+		
+	}
+	
+	
 
 }
