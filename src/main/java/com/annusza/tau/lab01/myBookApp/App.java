c4687e5fc859446e95ec8acc8bbd2aca76492c1e
Annusza <polonistyka2@o2.pl>
2019-10-10 15:10:25 +0200

    starting

---
+++ src/main/java/com/annusza/tau/lab01/myBookApp/App.java
@@ -0,0 +1,13 @@
+package com.annusza.tau.lab01.myBookApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
